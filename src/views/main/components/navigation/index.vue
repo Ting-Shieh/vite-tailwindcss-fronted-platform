e1fd3073f4995523c4cdfc41991d7104d6174023
Ting-Shieh <zsting29@gmail.com>
2022-10-31 00:21:46 +0800

    移動端移動端navigationBar 處理數據與視圖

--- src/views/main/components/navigation/index.vue
+++ src/views/main/components/navigation/index.vue
@@ -1,15 +1,19 @@
 <template>
-  <mobile-navigation v-if="isMobileTerminal"></mobile-navigation>
+  <mobile-navigation v-if="isMobileTerminal" :data="categoryData"></mobile-navigation>
 </template>
 <script setup>
 import { isMobileTerminal } from '@/utils/flexible.js'
 import mobileNavigation from './mobile/index.vue'
 import { getCategory }from '@/api/category.js'
-
+import { ref } from 'vue'
+// data
+const categoryData = ref([])
 // methods
 const getCategoryData = async () => {
-  const res = await getCategory()
-  console.log(res)
+  const { categorys } = await getCategory()
+  // console.log(category)
+  categoryData.value = categorys
 }
+// run
 getCategoryData()
 </script>
