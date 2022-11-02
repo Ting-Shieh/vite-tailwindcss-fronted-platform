74fe250cdedc16222fb759e91016c30ce2b12874
Ting-Shieh <zsting29@gmail.com>
2022-11-02 23:17:46 +0800

    slider 進行切換，當前位置為通過 translateX 進行指定，故只需計算出當前點擊item位置與寬度即可。

--- src/views/main/components/navigation/mobile/index.vue
+++ src/views/main/components/navigation/mobile/index.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="bg-white sticky top-0 left-0 z-10">
-    <ul class="relative flex overflow-x-auto p-1 text-xs text-zinc-600 overflow-hidden">
+    <ul ref="ulRef" class="relative flex overflow-x-auto p-1 text-xs text-zinc-600 overflow-hidden">
       <!-- 滑塊 -->
-      <li ref="sliderRef" class="this absolute bg-zinc-900 rounded-lg h-[22px] duration-200" :style="sliderStyle"></li>
+      <li ref="sliderRef" class="absolute bg-zinc-900 rounded-lg h-[22px] duration-200" :style="sliderStyle"></li>
       <!-- categoryItem class="last:mr-4" 指定最後一個距離右側的距離 -->
       <li
-        v-for="item in data"
+        :ref="setItemRef"
+        v-for="(item, index) in data"
         :key="item.id"
-        class="shrink-0 px-1.5 py-0.5 z-10 duration-200 last:mr-4">
+        class="shrink-0 px-1.5 py-0.5 z-10 duration-200 last:mr-4"
+        :class="setItemCls(index)"
+        @click="onItemClick(index)"
+        >
         {{ item.name }}
       </li>
       <!-- 漢堡按鈕 -->
@@ -18,19 +22,57 @@
   </div>
 </template>
 <script setup>
-import { ref } from 'vue';
-
+import { computed, onBeforeUpdate, ref, watch } from 'vue';
+import { useScroll } from '@vueuse/core'
 defineProps({
   data: {
     type: Array,
     required: true
   }
 })
-// 滑塊
+// data
+const ulRef = ref(null)
+const currentCategoryIndex = ref(0) // 選中 item 下標
+/** 滑塊 */
 const sliderStyle = ref({
   transform: `translateX(0px)`,
   width: '60px'
 })
+/** 獲取填充的所有 item 元素 */
+let itemRefs = []
+const setItemRef = (el) => {
+  if (el) {
+    itemRefs.push(el)
+  }
+}
+// update
+onBeforeUpdate(() => {
+  itemRefs = []
+})
+// hooks
+const { x: ulScrollLeft } = useScroll(ulRef) // 獲取 ul, 以計算偏移位置
+// computed
+const setItemCls = computed(() => (index) => {
+  return {
+    'text-zinc-100': currentCategoryIndex.value === index
+  }
+})
+// watch
+watch(currentCategoryIndex, (nV) => {
+  // 獲取選中元素的 left and width
+  const { left, width }= itemRefs[nV].getBoundingClientRect()
+  // 為 sliderStyle 設置屬性
+  sliderStyle.value = {
+    // ul 橫向滾動位置 + 當前元素的 left 偏移量
+    transform: `translateX(${ulScrollLeft.value + left - 10 + 'px'})`,
+    width: width + 'px'
+  }
+})
+// methods
+const onItemClick = (index) => {
+  currentCategoryIndex.value = index
+
+}
 </script>
 <style lang="scss" scoped>
 
