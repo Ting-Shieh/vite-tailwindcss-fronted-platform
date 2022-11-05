74c04011044799b40dcb4b5e6c457d103b430634
Ting-Shieh <zsting29@gmail.com>
2022-11-05 17:53:14 +0800

    vite 通用組建自動化註冊

--- src/libs/PopUp/index.vue
+++ src/libs/PopUp/index.vue
@@ -3,14 +3,25 @@
     <teleport to="body">
       <!-- 蒙版 -->
       <transition name="fade">
-        <div
+        <!-- old: 不可以在子組件直接修改響應式數據 -->
+        <!-- <div
           v-if="modelValue"
           @click="emits('update:modelValue', false)"
+          class="w-screen h-screen bg-zinc-900/80 fixed top-0 left-0"></div> -->
+        <!-- new: -->
+        <div
+          v-if="isOpen"
+          @click="isOpen = false"
           class="w-screen h-screen bg-zinc-900/80 fixed top-0 left-0"></div>
       </transition>
       <!-- 內容 -->
       <transition name="popup-down-up">
-        <div v-if="modelValue" v-bind="$attrs" class="w-screen bg-white z-50 fixed bottom-0">
+        <!-- old: 不可以在子組件直接修改響應式數據 -->
+        <!-- <div v-if="modelValue" v-bind="$attrs" class="w-screen bg-white z-50 fixed bottom-0">
+          <slot />
+        </div> -->
+        <!--  -->
+        <div v-if="isOpen" v-bind="$attrs" class="w-screen bg-white z-50 fixed bottom-0">
           <slot />
         </div>
       </transition>
@@ -18,21 +29,25 @@
   </div>
 </template>
 <script setup>
-import { useScrollLock } from '@vueuse/core';
+import { useScrollLock, useVModel } from '@vueuse/core';
 import { watch } from 'vue';
-
 
 const props = defineProps({
   modelValue: {
     required: true,
-    type: true
+    type: Boolean
   }
 })
-
-const emits = defineEmits(['update:modelValue'])
+// // old: 不需要主动触发了
+// const emits = defineEmits(['update:modelValue'])
+// new => 通過 useVModel 獲取到響應式數據 isOpen，當isOpen改變時，會自動觸發 update:modelValue
+const isOpen = useVModel(props)
 // 滾動鎖定
 const isLocked = useScrollLock(document.body)
-watch(() => props.modelValue, (nV) => {
+watch(
+  // () => props.modelValue,
+  isOpen,
+  (nV) => {
   isLocked.value = nV
 },{
   immediate: true
--- src/libs/index.js
+++ src/libs/index.js
@@ -1,9 +1,38 @@
-/** 完成通用組件註冊 */
-import SvgIcon from './SvgIcon/index.vue'
-import PopUp from './PopUp/index.vue'
+/** 完成通用組件“自動"註冊 */
+// import SvgIcon from './SvgIcon/index.vue'
+// import Popup from './Popup/index.vue'
+import { defineAsyncComponent } from 'vue'
+function isCapital(str){
+  let reg = /^[A-Z]+$/
+  return reg.test(str)
+}
+// https://blog.51cto.com/u_15302032/3065688
+function uncamelize(str, separator='-') {
+  let temp = str.replace(/[A-Z]/g, (match) => {
+    return separator + match.toLowerCase()
+  })
+  if(temp.slice(0,1) === separator){
+    temp = temp.slice(1)
+  }
+  return temp
+}
 export default {
   install (app) {
-    app.component('m-svg-icon', SvgIcon)
-    app.component('m-popup', PopUp)
+    // // old: 非自動註冊
+    // app.component('m-svg-icon', SvgIcon)
+    // app.component('m-popup', Popup)
+    // new: 自動註冊
+    // 1. 獲取當前路徑任意文件夾下的 index.vue 文件
+    const components = import.meta.glob('./*/index.vue')
+    // 2. 遍歷獲取到的組件模塊
+    for (const [fullpath, fn] of Object.entries(components)) {
+      // console.log(fullpath, '--', fn)
+      // 拼接組建註冊的 name
+      const tempName = fullpath.replace('./', '').split('/')[0]
+      // console.log(uncamelize(tempName))
+      const componentName = 'm-' + uncamelize(tempName)
+      // 通過 defineAsyncComponent 異步導入指定路徑下的組件
+      app.component(componentName, defineAsyncComponent(fn))
+    }
   }
 }
