a7b59ce7342861840d69eadda6d8c4bc51132de3
Ting-Shieh <zsting29@gmail.com>
2022-11-06 11:24:14 +0800

    pc 端Header模佈佈局架構補充說明．

--- src/views/layouts/components/header/index.vue
+++ src/views/layouts/components/header/index.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="w-full bg-white border-b border-b-zinc-200 px-2 py-1">
     <div class="flex items-center">
+      <!-- logo: 點擊返回首頁 -->
       <img
         class="h-4 cursor-pointer mr-2"
         src="https://m.imooc.com/static/wap/static/common/img/logo-small@2x.png"
         @click="onToHome"
       />
+      <!-- search:  搜尋區 -->
       <header-search class="mr-1"></header-search>
+      <!-- theme: 主題切換區 -->
       <header-theme class="mr-1"></header-theme>
+      <!-- my: 訊息區 -->
       <header-my></header-my>
     </div>
   </div>
