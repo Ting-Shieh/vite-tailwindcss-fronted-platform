47d5315e2f23b0018d7524518a29f7eb1a31a7a7
Ting-Shieh <zsting29@gmail.com>
2022-11-06 12:48:01 +0800

    通用組建 search 搜索框樣式處理-移動下拉區塊位置

--- src/libs/Search/index.vue
+++ src/libs/Search/index.vue
@@ -7,27 +7,28 @@
       <input
         placeholder="搜索"
         type="text"
-        class="block bg-zinc-100 caret-zinc-400 rounded-xl w-full h-[44px] pl-4 text-sm outline-0 tracking-wide font-semibold border border-zinc-100 duration-500 group-hover:border-zinc-200 focus:border-red-300"
+        class="block bg-zinc-100 caret-zinc-400 rounded-xl w-full h-[44px] pl-4 text-sm outline-0 tracking-wide font-semibold border border-zinc-100 duration-500 group-hover:bg-white group-hover:border-zinc-200 focus:border-red-300"
       />
       <!-- 刪除按鈕 -->
       <m-svg-icon
         name="input-delete"
-        class="w-1.5 h-1.5 absolute translate-y-[-50%] top-[50%] right-9 duration-500 cursor-pointer"
+        class="w-1.5 h-1.5 this absolute translate-y-[-50%] top-[50%] right-9 duration-500 cursor-pointer"
       ></m-svg-icon>
       <!-- 分割線 -->
       <div
         class="opacity h-1.5 w-[1px] absolute translate-y-[-50%] top-[50%] right-[62px] duration-500 bg-zinc-200 group-hover:opacity-100"
       ></div>
+      <!-- TODO: 搜尋按鈕（通用組建） -->
     </div>
+    <!-- 下拉區 -->
+    <transition name="slider">
+      <div
+        class="max-h-[368px] w-full text-base overflow-auto bg-white absolute left-0 top-[56px] p-2 rounded z-20 border border-zinc-200 duration-200 hover:shadow-3xl"
+      > 
+        <slot name="dropdown" />
+      </div>
+    </transition>
   </div>
-  <!-- 下拉區 -->
-  <transition name="slider">
-    <div
-      class="max-h-[368px] w-full text-base overflow-auto bg-white absolute left-0 top-[56px] p-2 rounded z-20 border border-zinc-200 duration-200 hover:shadow-3xl"
-    > 
-      <slot name="dropdown" />
-    </div>
-  </transition>
 </template>
 <script setup></script>
 <style lang="scss" scoped>
