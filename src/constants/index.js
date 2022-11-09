8a403efa941d3d0389c35e612137338ea7f16d41
Ting-Shieh <zsting29@gmail.com>
2022-11-09 19:47:12 +0800

    popover 氣泡卡片基礎功能實現

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,3 +4,9 @@ export const ALL_CATEGORY_ITEM = {
   id: 'all',
   name: '全部'
 }
+// 暗黑主題
+export const THEME_DARK = 'dark'
+// 淺色主題
+export const THEME_LIGHT = 'light'
+// 系統主題
+export const THEME_SYSTEM = 'system'
--- src/views/layouts/components/header/HeaderTheme.vue
+++ src/views/layouts/components/header/HeaderTheme.vue
@@ -1,3 +1,52 @@
 <template>
-  <div class="">theme</div>
+  <m-popover class="">
+    <template #reference>
+      <m-svg-icon
+        name="theme-light"
+        fillClass="fill-zinc-900"
+        class="guide-theme w-4 h-4 p-1 cursor-pointer rounded-sm duration-200 outline-none hover:bg-zinc-100/60"
+      ></m-svg-icon>
+    </template>
+    <div class="w-[140px] overflow-hidden">
+      <div
+        class="flex items-center p-1 cursor-pointer rounded hover:bg-zinc-100/60"
+        v-for="item in themeArr"
+        :key="item.id"
+      >
+        <m-svg-icon
+          :name="item.icon"
+          class="w-1.5 h-1.5 mr-1"
+          fillClass="fill-zinc-900"
+        ></m-svg-icon>
+        <span class="text-zinc-800 text-sm">{{ item.name }}</span>
+      </div>
+    </div>
+  </m-popover>
 </template>
+<script setup>
+import { THEME_SYSTEM, THEME_LIGHT, THEME_DARK } from '@/constant/index.js'
+import {} from 'vue'
+// 建構渲染數據源
+const themeArr = [
+  {
+    id: '0',
+    type: THEME_LIGHT,
+    icon: 'theme-light',
+    name: '極簡白'
+  },
+  {
+    id: '1',
+    type: THEME_DARK,
+    icon: 'theme-dark',
+    name: '極簡黑'
+  },
+  {
+    id: '2',
+    type: THEME_SYSTEM,
+    icon: 'theme-system',
+    name: '跟隨系統'
+  }
+]
+</script>
+<style lang="scss" scoped>
+</style>
