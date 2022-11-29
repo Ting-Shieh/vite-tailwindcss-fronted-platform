fddf0f4c52e458d206b0afeb24d0632150c2018e
Ting-Shieh <zsting29@gmail.com>
2022-11-29 21:59:59 +0800

    建立長列表 infinite組建視圖與邏輯

---
+++ src/libs/InfiniteList/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="">
+    <!-- 內容 -->
+    <slot></slot>
+    <div ref="loadingRef" class="h-6 py-4">
+      <!-- 加載更多 -->
+      <m-svg-icon
+      v-show="loading"
+      class="w-4 h-4 mx-auto animate-spin"
+      name="infinite-load"
+      ></m-svg-icon>
+      <!-- 沒有更多數據 -->
+      <p v-if="isFinished" class="text-center text-base text-zinc-400">沒有更多數據</p>
+    </div>
+  </div>
+</template>
+<script setup>
+import { useVModel } from '@vueuse/core'
+import { ref } from 'vue'
+
+const props = defineProps({
+  // 是否處於加載狀態
+  modelValue: {
+    type: Boolean,
+    required: true
+  },
+  // 數據是否全部加載完成
+  isFinished: {
+    type: Boolean,
+    default: false
+  }
+})
+const emits = defineEmits(['onLoad', 'update:modelValue'])
+// 處理loading狀態
+const loading = useVModel(props)
+
+const loadingRef = ref(null)
+</script>
+<style lang="scss" scoped>
+
+</style>
